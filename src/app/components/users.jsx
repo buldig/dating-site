60022b96170482a13ef144a6d4ff0e86afbf0d60
buldig <mr.karandashin@mail.ru>
2022-06-12 10:46:55 +0300

    add filtration users and refactoring

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -1,47 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Pagination from "./pagination";
-import User from "./user";
 import paginate from "../utils/paginate";
 import PropTypes from "prop-types";
-
+import GroupList from "./groupList";
+import api from "../api";
+import SearchStatus from "./searchStatus";
+import UserTable from "./usersTable";
 const Users = ({ users, ...rest }) => {
-  const count = users.length;
-  const pageSize = 4;
+  const pageSize = 2;
   const [currentPage, setCurrentPage] = useState(1);
+  const [professions, setProfession] = useState();
+  const [selectedProf, setSelectedProf] = useState();
+  useEffect(() => {
+    api.professions.fetchAll().then((data) => setProfession(data));
+  }, []);
   const handlePageChange = (pageIndex) => {
     setCurrentPage(pageIndex);
   };
-
-  const userCrop = paginate(users, currentPage, pageSize);
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [selectedProf]);
+  const handleProffesionSelect = (item) => {
+    setSelectedProf(item);
+  };
+  const clearFilter = () => {
+    setSelectedProf(undefined);
+  };
+  const handleSort = (item) => {
+    console.log(item);
+  };
+  let filteredUsers;
+  if (selectedProf) {
+    filteredUsers = users.filter(
+      (user) => JSON.stringify(user.profession) === JSON.stringify(selectedProf)
+    );
+  } else filteredUsers = users;
+  const userCrop = paginate(filteredUsers, currentPage, pageSize);
+  const count = filteredUsers.length;
   return (
-    <>
-      {count > 0 && (
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Имя</th>
-              <th scope="col">Качества</th>
-              <th scope="col">Профессия</th>
-              <th scope="col">Встретился, раз</th>
-              <th scope="col">Оценка</th>
-              <th scope="col">Избранное</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            {userCrop.map((user) => (
-              <User key={user._id} {...rest} {...user} />
-            ))}
-          </tbody>
-        </table>
+    <div className="d-flex">
+      {professions && (
+        <div className="d-flex flex-column p-3">
+          <GroupList
+            items={professions}
+            onItemSelect={handleProffesionSelect}
+            selectedItem={selectedProf}
+          />
+
+          <button className="btn btn-primary mt-2" onClick={clearFilter}>
+            Сбросить
+          </button>
+        </div>
       )}
-      <Pagination
-        itemsCount={count}
-        pageSize={pageSize}
-        currentPage={currentPage}
-        onPageChange={handlePageChange}
-      />
-    </>
+      <div className="d-flex flex-column">
+        <SearchStatus length={count} />
+        {count > 0 && (
+          <UserTable users={userCrop} onSort={handleSort} {...rest} />
+        )}
+        <div className="d-flex justify-content-center">
+          <Pagination
+            itemsCount={count}
+            pageSize={pageSize}
+            currentPage={currentPage}
+            onPageChange={handlePageChange}
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 
